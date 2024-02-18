db53a812ea2f2ed3d3373aaf537e2a0c7f9461ee
= <cole.barham98@gmail.com>
2024-02-18 16:02:55 -0500

    a

--- commands/ping.js
+++ commands/ping.js
@@ -4,12 +4,12 @@ module.exports = {
         description: 'Get ping of the bot',
         rnk: 'Admin',
         cmdargs: [
-            {name: 'message', type: 'string', required: true},
+            {name: 'message', type: 'string', required: false},
         ]
     },
     async run (bot, message, args) {
         if (args.message) {
-            message.channel.send(`${args.message} \`" + ${bot.ws.ping} + " ms\``);
+            message.channel.send(`${args.message} \` ${bot.ws.ping} ms``);
         } else {
             message.channel.send("Pong! \`" + bot.ws.ping + " ms\`");
         }
