4040d90378d1ff95f71a9795e92d70b6e2172101
= <cole.barham98@gmail.com>
2024-02-18 14:40:01 -0500

    test

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require('dotenv').config();
 const { prefix } = require("./config.json");
 
 const { Client, Intents, Collection } = require('discord.js');
--- commands/ping.js
+++ commands/ping.js
@@ -3,6 +3,7 @@ module.exports = {
         name: 'ping',
         description: 'Get ping of the bot',
         usage: `!ping`,
+        rnk: 'Admin',
     },
     async run (bot,message,args) {
         message.channel.send("My ping is \`" + bot.ws.ping + " ms\`");
