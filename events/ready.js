06a6c81de6115180d6d4e80b428dda4c627ccc60
= <cole.barham98@gmail.com>
2024-02-18 14:32:49 -0500

    --

--- events/ready.js
+++ events/ready.js
@@ -4,7 +4,7 @@ module.exports = {
     execute(bot) {
         //Log Bot's username and the amount of servers its in to console
         console.log(`${bot.user.username} is online on ${bot.guilds.cache.size} servers!`);
-
+        console.log('Bot Online')
         //Set the Presence of the bot user
         bot.user.setPresence({ activities: [{ name: 'My code'}] });
     }
