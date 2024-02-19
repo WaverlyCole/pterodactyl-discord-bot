efdf96b98eb901493106cf84b3c666e57a114c0a
= <cole.barham98@gmail.com>
2024-02-19 10:51:05 -0500

    delkey

--- commands/pterodactyl/delkey.js
+++ commands/pterodactyl/delkey.js
@@ -9,7 +9,7 @@ module.exports = {
         message.reply("Removing key...")
             .then(async sentMessage => {
                 if (await bot.pterodactylkeys.has(message.author.id)) {
-                    await bot.pterodactyl.delete(message.author.id)
+                    await bot.pterodactylkeys.delete(message.author.id)
                     sentMessage.edit("Key removed.")
                 } else {
                     sentMessage.edit("You have no key set!")
