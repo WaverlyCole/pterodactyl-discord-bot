fee6b0a754dd3524aaf2b1de1b52c8a253546615
= <cole.barham98@gmail.com>
2024-03-02 21:44:43 -0500

    a

--- commands/pterodactyl/listservers.js
+++ commands/pterodactyl/listservers.js
@@ -38,9 +38,19 @@ module.exports = {
                     .setDescription("Here is a list of all servers you currently have access to")
 
                 for (let key in allServers) {
-                        const status = await pterodactyl.getrunningstate(bot, userAPIKey, allServers[key].identifier)
-                        embed.addField(`${key} (${allServers[key].identifier})`, status_lookup[status.current_state]);
-                }
+                    function round(num) {
+                        return Math.round(num * 10) / 10
+                    }
+
+                    const status = await pterodactyl.getrunningstate(bot, userAPIKey, allServers[key].identifier)
+                    const maxMem = allServers[key].limits.memory
+                    const currMem = status.resources.memory_bytes / 1024 / 1024
+                    const maxDisk = allServers[key].limits.disk
+                    const currDisk = status.resources.disk_bytes / 1024 / 1024
+                    const cpu = Math.round(status.resources.cpu_absolute)
+
+                    embed.addField(`${status_lookup[status.current_state]} ${key} (${allServers[key].identifier})`, `Mem: ${round(currMem/maxMem*100)}% Cpu: ${cpu}% Disk: ${round(currDisk/maxDisk*100)}%`, false);
+            }
 
                 sentMessage.edit({ content: ' ', embeds: [embed] })
             })
