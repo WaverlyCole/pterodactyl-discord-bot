617459db7b9ad4338e0466db317d6fa1b0f86dfe
= <cole.barham98@gmail.com>
2024-03-03 21:35:43 -0500

    a

--- commands/pterodactyl/restartserver.js
+++ commands/pterodactyl/restartserver.js
@@ -82,6 +82,7 @@ module.exports = {
                                 sentMessage.edit({ content: ' ', embeds: [embed] })
                                 await new Promise(resolve => setTimeout(resolve, 1000));
                             }
+                            break;
                         } else {
                             sentMessage.edit({content: "This server is in the process of starting/stopping and cannot be restarted at this moment."})
                         }
